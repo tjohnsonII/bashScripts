024bd6f512df0d38660b13fa27dc697658c02aa9
Timothy A Johnson II <tjohnson082083@gmail.com>
2023-03-13 11:57:22 -0400

    On branch master  Changes to be committed: 	new file:   Create_A_User_And_Group/Create_A_User_And_Group.xcodeproj/project.pbxproj 	new file:   Create_A_User_And_Group/Create_A_User_And_Group.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   Create_A_User_And_Group/Create_A_User_And_Group.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   Create_A_User_And_Group/Create_A_User_And_Group.xcodeproj/project.xcworkspace/xcuserdata/tim2.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Create_A_User_And_Group/Create_A_User_And_Group.xcodeproj/xcuserdata/tim2.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Create_A_User_And_Group/Create_A_User_And_Group/main.c

---
+++ Create_A_User_And_Group/Create_A_User_And_Group/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Create_A_User_And_Group
+//
+//  Created by Timothy Johnson on 2/16/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
